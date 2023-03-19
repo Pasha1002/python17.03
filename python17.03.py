5101101c5b6aa291db5990e24b362d076393116a
Pasha1002 <kanavetspavel2001@gmail.com>
2023-03-19 14:50:41 +0200

    tusk dune

--- python17.03.py
+++ python17.03.py
@@ -1,1 +1,7 @@
+num_1 = int(input("Input number 1: "))
 
+num_2 = int(input("Input number 2: "))
+
+print(f"Result: { num_1 + num_2}")
+print(f"Result: { num_1 - num_2}")
+print(f"Result: { num_1 * num_2}")
